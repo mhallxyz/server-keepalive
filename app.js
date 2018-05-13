f6f9d1e2171c4128d12dbc7b55db8695d8a48f0e
Mathew Hall <m@mhall.xyz>
2018-05-13 18:56:13 +0100

    Log added to confirm script running

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const target = "http://mhall.xyz";
 
 
 console.log("Started");
+
 setInterval(function(){
 fetchUrl(target, function(error, meta, body){
     console.log(body.toString());
 });
 }, minutes * 60 * 1000);
+
+setInterval(function(){
+    console.log("server-keepalive running")
+}, 5000);
